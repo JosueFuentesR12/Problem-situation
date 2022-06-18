165be715d3e945c99cf791020cbceb52a1f9b757
JosueFuentesR12 <105233426+JosueFuentesR12@users.noreply.github.com>
2022-06-17 22:57:58 -0500

    Final Code
    Code C++

--- Video.h
+++ Video.h
@@ -1,12 +1,24 @@
+#pragma once
 #include <iostream>
+#include <String.h>
+
+//Carlos Alberto Mendoza Medina A01661896 
+//Jonathan Josué Fuentes Ramírez A01661894
 
 class Video{
+    public:    
     int id;
     std::string nombre;
     float duracion;
     float rating;
     std::string genre;
-    public:
+    Video(){
+        id = 0;
+        nombre = "#";
+        duracion = 0.0;
+        rating = 0.0;
+        genre = "#";
+    };
     Video(int elId, std::string elNombre, float laDuracion, std::string elGenre,float rate){
         id = elId;
         nombre = elNombre;
